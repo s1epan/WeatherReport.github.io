a1bf61bf3acfd9d9c58fe8e28b3c6cdf6383e9f3
Stepan <anergaryan@mail.ru>
2024-04-03 11:57:37 +0300

    сделал примерное местоположение элементов в компоненте ContentLeftBar

--- src/components/ContentLeftBar/ContentLeftBar.jsx
+++ src/components/ContentLeftBar/ContentLeftBar.jsx
@@ -1,7 +1,15 @@
 import "./ContentLeftBar.css";
 
 function ContentLeftBar() {
-  return <div className="content-left-bar"></div>;
+  return (
+    <div className="content-left-bar">
+      <div className="left-bar-elemets">
+        <div className="left-bar-img">ICON</div>
+        <p className="left-bar-temp">25°С</p>
+        <p className="left-bar-location-data">Moscow | 03.04.2024</p>
+      </div>
+    </div>
+  );
 }
 
 export default ContentLeftBar;
